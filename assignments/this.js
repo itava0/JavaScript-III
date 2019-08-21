a58e67c15c7471968304f338b48ddc5661c7169c
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-08-21 17:04:04 -0400

    Finished Problems on this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. On global binding the 'this' keyword refers to window. 
+* 2. On Implicit binding the 'this' keyword refers to pronoun meaning when a function is called by a proceding dot.
+* 3. On explicit  binding the 'this' keyword refers to the function with call or apply method.
+* 4. On new Binding the 'this' keyword refers to object that is created and return by the constructor.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,53 @@
 // Principle 1
 
 // code example for Window Binding
+function welcome(name) {
+  console.log(this);
+  return name;
+}
 
+welcome('jame');
 // Principle 2
 
 // code example for Implicit Binding
+const car = {
+  year: 2016,
+  make: 'Toyota',
+  model: 'camry',
+  own:function (year) {
+    console.log(`I own a ${year} ${this.make} ${this.model}`)
+  }
+};
+
+car.own(2016);
 
 // Principle 3
 
 // code example for New Binding
 
+
+function Owncar(car){
+  this.car = car;
+}
+
+let Mycar = new Owncar('I own a 2016 Toyota Camry');
+
+console.log(Mycar.car);
+
 // Principle 4
 
 // code example for Explicit Binding
+
+ function prevCar(){
+  console.log(this.make);
+}
+
+let lovelyOldCar ={
+  year: 1999,
+  make: 'Nissan',
+  model: 'Sentra',
+}
+
+prevCar.call(lovelyOldCar);
+
+
